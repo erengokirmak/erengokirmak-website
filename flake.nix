116ff3785cd6ea34536c26d46bc46bbc038d1b36
Eren Gökırmak <sukruerengokirmak@gmail.com>
2025-09-28 19:07:35 +0300

    Fix flake.nix name error

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "drgokirmak.com dev environment flake";
+  description = "erengokirmak.com dev environment flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
